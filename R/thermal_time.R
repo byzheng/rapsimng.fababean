7dbcaf0b62934a4fecc9b282129724d22dc5e1fd
Bangyou Zheng (laptop) <zheng.bangyou@gmail.com>
2026-08-28 14:44:19 +1000

    Add thermal time method option to fababean model and update documentation

--- R/thermal_time.R
+++ R/thermal_time.R
@@ -23,7 +23,7 @@ thermal_time <- function(mint,
     maxt,
     x_temp = fababean$get("phenology.thermal_time.x"), 
     y_temp = fababean$get("phenology.thermal_time.y"),
-    method = "HourlySinPpAdjusted",
+    method = fababean$get("phenology.thermal_time.method"),
     ...) {
     
     tidyweather::thermal_time(
