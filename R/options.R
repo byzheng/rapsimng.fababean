fb028552698952007aa2eaf9b8ce2c16115b1034
Bangyou Zheng (laptop) <zheng.bangyou@gmail.com>
2026-09-14 15:21:58 +1000

    Add return value documentation for fababean options manager

--- R/options.R
+++ R/options.R
@@ -22,6 +22,7 @@
 #' @export
 #' @importFrom optree create_options_manager
 #' @importFrom optree v_xypair
+#' @return An option manager object for configuring fababean model parameters.
 #' @examples
 #' # Get default thermal time cardinal temperatures
 #' fababean$get("phenology.thermal_time.x")
